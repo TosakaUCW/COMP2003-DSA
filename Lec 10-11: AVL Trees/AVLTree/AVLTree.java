1bee58ee050f1216efdf94b32912ba4b8d34c92a
TosakaUCW <tosakaucw0@gmail.com>
2024-11-26 04:47:18 +0800

    main: Added

--- Lec 10-11: AVL Trees/AVLTree/AVLTree.java
+++ Lec 10-11: AVL Trees/AVLTree/AVLTree.java
@@ -119,6 +119,63 @@ public class AVLTree extends BST{
 		super();
 	}
 
+    // Get the balance factor of a node
+    private int getBalance(Node node) {
+        return node == null ? 0 : Node.getHeight(node.getLeft()) - Node.getHeight(node.getRight());
+    }
+	
+	private Node rotateRight(Node y) {
+        Node x = y.getLeft();
+        Node T2 = x.getRight();
+
+        x.setRight(y);
+        y.setLeft(T2);
+
+        y.updateHeight();
+        x.updateHeight();
+
+        return x;
+    }
+	
+	private Node rotateLeft(Node x) {
+        Node y = x.getRight();
+        Node T2 = y.getLeft();
+
+        y.setLeft(x);
+        x.setRight(T2);
+
+        x.updateHeight();
+        y.updateHeight();
+
+        return y;
+    }
+	
+	private Node rebalance(Node root) {
+		int balance = getBalance(root);
+		
+		// If balanced, return
+		if (-2 < balance && balance < 2) {
+			return root;
+		}
+		
+		// If unbalanced, perform rotations
+		if (balance > 1 && getBalance(root.getLeft()) >= 0) { // Left-Left case
+            return rotateRight(root);
+        }
+        if (balance > 1 && getBalance(root.getLeft()) < 0) { // Left-Right case
+            root.setLeft(rotateLeft(root.getLeft()));
+            root = rotateRight(root);
+        }
+        if (balance < -1 && getBalance(root.getRight()) <= 0) { // Right-Right case
+            return rotateLeft(root);
+        }
+        if (balance < -1 && getBalance(root.getRight()) > 0) { // Right-Left case
+            root.setRight(rotateRight(root.getRight()));
+            root = rotateLeft(root);
+        }
+		
+		return root;
+	}
 
 	public Node insertNode(Node root, int x) {
 		/**
@@ -128,7 +185,13 @@ public class AVLTree extends BST{
 		 * output: the (updated) root of the subtree
 		 */
 		root = super.insertNode(root, x);
-		/*YOUR CODE HERE*/
+		
+		// Update the height of the current node
+		root.updateHeight();
+		
+		// Rebalance the tree
+		root = rebalance(root);
+		
 		return root;
 	}
 
@@ -140,7 +203,12 @@ public class AVLTree extends BST{
 		 * output: the (updated) root of the subtree
 		 */
 		root = super.deleteNode(root, x);
-		/*YOUR CODE HERE*/
+		
+		if (root != null) {
+			root.updateHeight();
+			root = rebalance(root);
+		}
+		
 		return root;
 	}
 
